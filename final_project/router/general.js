0ee319093a263fe047f12e98bdadde6cfc5fe8f7
Tomleta <tholet26@gmail.com>
2025-03-14 16:42:29 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,10 +10,13 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", async (req, res) => {
+  try {
+    const allBooks = await getAllBooks();
+    return res.status(200).send(JSON.stringify(allBooks, null, 4));
+  } catch (e) {
+    res.status(500).send(e);
+  }
 });
 
 // Get book details based on ISBN
